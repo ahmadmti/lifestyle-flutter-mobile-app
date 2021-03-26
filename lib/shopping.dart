fd5d4c4d6d73a61985792ca74c158797929cb1ee
irfan-429 <iakramdeveloper@gmail.com>
2021-03-27 04:12:34 +0500

    task 5 done

--- lib/shopping.dart
+++ lib/shopping.dart
@@ -1,3 +1,4 @@
+import 'package:custom_tab/custom_tab.dart';
 import 'package:flutter/material.dart';
 import 'package:lifestyle/userAccount.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -14,138 +15,141 @@ class shopping extends StatefulWidget {
 }
 
 class shoppingState extends State<shopping> {
+  List<ShopingPojo> pojo = [];
+  List<Brand> brands = [];
+  var man = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSuvpWGcijPlOi4c3KIbq7hkiIu5x7kY9x9w&usqp=CAU";
+  var woman = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQxnm1NuCwL1NR6r4TlDV-aEbMOfsSCBAZIw&usqp=CAU";
+  var kid = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPsHbpiok68WdJ2hEE5XPRUOKWg9w6IiDktA&usqp=CAU";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    brands.add(Brand(
+        wear: "assets/shopping_image/gucci_shopping.jpg", site: "https://www.gucci.com/ae/en_gb/", name: "Gucci"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/georgio_armani_logo.png",
+        site: "https://www.armani.com/wx/armanicom",
+        name: "Armani"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/splash_logo.png", site: "https://www.splashfashions.com/ae/en", name: "Splash"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/max_logo.png", site: "https://www.maxfashion.com/ae/en", name: "Max Fashion"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/american_eagle_outfit.jpg",
+        site: "https://www.americaneagle.ae/en/",
+        name: "American Eagle"));
+    brands.add(Brand(wear: "assets/shopping_image/nike_logo.png", site: "https://www.nike.com/ae/", name: "Nike"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/gucci_shopping.jpg", site: "https://www.gucci.com/ae/en_gb/", name: "Gucci"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/georgio_armani_logo.png",
+        site: "https://www.armani.com/wx/armanicom",
+        name: "Armani"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/splash_logo.png", site: "https://www.splashfashions.com/ae/en", name: "Splash"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/max_logo.png", site: "https://www.maxfashion.com/ae/en", name: "Max Fashion"));
+    brands.add(Brand(
+        wear: "assets/shopping_image/american_eagle_outfit.jpg",
+        site: "https://www.americaneagle.ae/en/",
+        name: "American Eagle"));
+    brands.add(Brand(wear: "assets/shopping_image/nike_logo.png", site: "https://www.nike.com/ae/", name: "Nike"));
+    pojo.add(ShopingPojo(gender: "MEN", img: man, brands: brands)); //add
+    pojo.add(ShopingPojo(gender: "WOMEN", img: woman, brands: brands)); //add
+    pojo.add(ShopingPojo(gender: "KIDS", img: kid, brands: brands)); //add
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Scaffold(
-        appBar: AppBar(
-          title: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Text("Shopping"),
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            color: Colors.white54,
-            child: Center(
-              child: Column(
-                children: [
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        url_launcher("https://www.gucci.com/ae/en_gb/");
-                      },
-                      child: Image.asset(
-                          "assets/shopping_image/gucci_shopping.jpg",
-                          height: 200.0,
-                          width: 350.0),
-                    ),
-                  ),
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        url_launcher("https://www.armani.com/wx/armanicom");
-                      },
-                      child: Image.asset(
-                        "assets/shopping_image/georgio_armani_logo.png",
-                        height: 200.0,
-                        width: 350.0,
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        // Perform some action
-                        url_launcher("https://www.splashfashions.com/ae/en");
-                      },
-                      child: Image.asset(
-                        "assets/shopping_image/splash_logo.png",
-                        height: 200.0,
-                        width: 350.0,
-                      ),
-                    ),
-                  ),
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        // Perform some action
-                        url_launcher("https://www.maxfashion.com/ae/en");
-                      },
-                      child: Image.asset(
-                        "assets/shopping_image/max_logo.png",
-                        height: 200.0,
-                        width: 350.0,
-                      ),
-                    ),
-                  ),
-
-                  //CHANGE FROM HERE
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        url_launcher("https://www.americaneagle.ae/en/");
-                      },
-                      child: Image.asset(
-                          "assets/shopping_image/american_eagle_outfit.jpg",
-                          height: 200.0,
-                          width: 350.0),
-                    ),
-                  ),
-                  Card(
-                    child: InkWell(
-                      onTap: () {
-                        url_launcher("https://www.nike.com/ae/");
-                      },
-                      child: Image.asset(
-                        "assets/shopping_image/nike_logo.png",
-                        height: 200.0,
-                        width: 350.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+          appBar: AppBar(
+            title: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Text("Shopping"),
             ),
           ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          shape: CircularNotchedRectangle(),
-          child: Container(
-            height: 75,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                IconButton(
-                  iconSize: 50.0,
-                  icon: Icon(Icons.settings),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => settings()));
-                  },
+          body: CustomTab(
+            3,
+            (i) => new ClipRRect(
+              borderRadius: BorderRadius.circular(10.0),
+              child: new Stack(fit: StackFit.expand, children: [
+                Image.network(
+                  pojo[i].img,
+                  fit: BoxFit.fill,
                 ),
-                IconButton(
-                  iconSize: 50.0,
-                  icon: Icon(Icons.home),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => mainHome()));
-                  },
-                ),
-                IconButton(
-                  iconSize: 50.0,
-                  icon: Icon(Icons.supervised_user_circle_outlined),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => userAccount()));
-                  },
-                )
-              ],
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: new Padding(
+                      padding: EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        pojo[i].gender,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            background: Paint()..color = Colors.black.withAlpha(900000),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ))
+              ]),
             ),
-          ),
-        ),
-      ),
+            (context, i) => new GridView.builder(
+                padding: EdgeInsets.all(5.0),
+                itemCount: pojo[i].brands.length,
+                // shrinkWrap: true,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0,
+                ),
+                itemBuilder: (context, j) => Column(
+                      // borderRadius: BorderRadius.circular(10.0),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () => launchBrowser(pojo[i].brands[j].site),
+                          child: Container(
+                            height: 140,
+                            child: Card(
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              child: new Image.asset(
+                                pojo[i].brands[j].wear,
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(pojo[i].brands[j].name),
+                      ],
+                    )),
+            indicatorColor: Colors.blue,
+          )),
     );
   }
+
+  launchBrowser(url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+class ShopingPojo {
+  String gender, img;
+  List<Brand> brands;
+
+  ShopingPojo({this.gender, this.img, this.brands});
+}
+
+class Brand {
+  String wear, site, name;
+
+  Brand({this.wear, this.site, this.name});
 }
